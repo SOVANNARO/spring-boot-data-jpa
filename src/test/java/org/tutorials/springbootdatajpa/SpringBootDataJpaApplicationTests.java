da530584e86f3a8a5dec0088535b8385b923b6ac
SOVANNARO <sovannaro066@gmail.com>
2024-06-05 22:57:21 +0700

    add: init project

---
+++ src/test/java/org/tutorials/springbootdatajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tutorials.springbootdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
